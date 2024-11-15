5eaa6c47420d044b54f9074c64ca6c347aab3f75
William Sylvestre <farakwarts@gmail.com>
2024-11-15 14:21:00 -0500

    Update Indice

--- Assets/Scripts/Clue.cs
+++ Assets/Scripts/Clue.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class Clue : MonoBehaviour
 {
     MeshRenderer shaderMesh;
+    public ParticleSystem particles;
     float dissolveValue = 1f;
     public bool isActif = false;
+    bool isFound = false;
 
     private void Update()
     {
@@ -12,6 +14,11 @@ public class Clue : MonoBehaviour
         shaderMesh.material.SetFloat("_DissolveValue", dissolveValue);
         if (isActif && dissolveValue > 0.3)
         {
+            if (!isFound)
+            {
+                particles.Play();
+                isFound = true;
+            }
             dissolveValue -= (0.02f * Time.deltaTime);
         }
     }
