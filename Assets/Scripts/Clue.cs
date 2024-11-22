627a564e5befbb26e6c21dc52cabe18b5f23f508
William Sylvestre <farakwarts@gmail.com>
2024-11-22 11:12:34 -0500

    Update IndicePrefab & Clue Script
    Add fairy sings to help the player to find the runes

--- Assets/Scripts/Clue.cs
+++ Assets/Scripts/Clue.cs
@@ -7,23 +7,35 @@ public class Clue : MonoBehaviour
     public AudioSource foundClue;
     AudioSource clueAppearAudio;
 
+    public AudioSource fairySings;
+    Enigme enigme;
+
     float dissolveValue = 1f;
     public bool isActif = false;
     bool isFound = false;
 
-    float fadeDuration = 5f;
     float fadeStart = 2f;
-    public float initialVolume;
-    float timer;
+    public float initialAppearVol;
+    public float initialSingsVol;
+
+    float timerClue = 0f;
+    float timerSing = 0f;
 
     private void Start()
     {
+        enigme = FindAnyObjectByType<Enigme>();
         clueAppearAudio = GetComponent<AudioSource>();
-        initialVolume = clueAppearAudio.volume;
+        initialAppearVol = clueAppearAudio.volume;
+        initialSingsVol = fairySings.volume;
     }
 
     private void Update()
     {
+        if (enigme.hasNote)
+        {
+            fairySings.Play();
+        }
+
         shaderMesh = GetComponent<MeshRenderer>();
         shaderMesh.material.SetFloat("_DissolveValue", dissolveValue);
         if (isActif && dissolveValue > 0.3)
@@ -42,19 +54,32 @@ public class Clue : MonoBehaviour
         if (isFound)
         {
             ClueAppeared();
+            StopSing();
         }
     }
 
     private void ClueAppeared()
     {
-        timer += Time.deltaTime;
-        if (timer >= fadeStart)
+        timerClue += Time.deltaTime;
+        if (timerClue >= fadeStart)
         {
-            clueAppearAudio.volume = Mathf.Lerp(initialVolume, 0f, (timer - fadeStart) / fadeDuration);
-
+            clueAppearAudio.volume = Mathf.Lerp(initialAppearVol, 0f, (timerClue - fadeStart) / 5f);
             if (clueAppearAudio.volume <= 0f)
             {
                 clueAppearAudio.Stop();
+            }
+        }
+    }
+
+    private void StopSing()
+    {
+        timerSing += Time.deltaTime;
+        if (timerSing >= fadeStart)
+        {
+            fairySings.volume = Mathf.Lerp(initialSingsVol, 0f, (timerSing - fadeStart) / 2f);
+            if (fairySings.volume <= 0f)
+            {
+                fairySings.Stop();
             }
         }
     }
