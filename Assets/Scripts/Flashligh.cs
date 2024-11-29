945b4eb333a775208d62932709c9862d1e448692
William Sylvestre <farakwarts@gmail.com>
2024-11-29 09:10:14 -0500

    Add Flashlight

---
+++ Assets/Scripts/Flashligh.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+
+public class Flashligh : MonoBehaviour
+{
+    [SerializeField] GameObject lightObj;
+    public AudioClip[] interactSounds;
+    AudioSource audioSource;
+
+    LightSwitch lightSwitch;
+    InsideOrOutside playerLocationState;
+    bool isOn;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        lightSwitch = FindAnyObjectByType<LightSwitch>();
+        playerLocationState = FindAnyObjectByType<InsideOrOutside>();
+    }
+
+    private void Update()
+    {
+        if (!lightSwitch.switchOn || !playerLocationState.isInside)
+        {
+            if (!isOn)
+            {
+                StartCoroutine(FlashlightState(true, 0));
+            }
+        }
+        else
+        {
+            if (isOn)
+            {
+                StartCoroutine(FlashlightState(false, 1));
+            }
+        }
+    }
+
+    IEnumerator FlashlightState(bool state, int index)
+    {
+        isOn = state;
+        yield return new WaitForSeconds(0.2f);
+        audioSource.clip = interactSounds[index];
+        audioSource.Play();
+        yield return new WaitForSeconds(0.1f);
+        lightObj.SetActive(state);
+    }
+}
