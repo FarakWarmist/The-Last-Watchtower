e9533aa7966472a9738206484b2484289098da51
William Sylvestre <farakwarts@gmail.com>
2024-11-29 09:11:41 -0500

    Add MonsterFootsteps Script

---
+++ Assets/Scripts/MonsterFootsteps.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class MonsterFootsteps : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] footstepsClips;
+    public Monster thisMonster;
+    bool startSound;
+
+
+    private void Update()
+    {
+        if (thisMonster.monster.velocity != Vector3.zero && !startSound)
+        {
+            StartCoroutine(MoveSound());
+        }
+    }
+
+    IEnumerator MoveSound()
+    {
+        startSound = true;
+        audioSource.clip = footstepsClips[Random.Range(0, footstepsClips.Length)];
+        audioSource.Play();
+        yield return new WaitForSeconds(0.28f);
+        audioSource.Stop();
+        startSound = false;
+    }
+}
