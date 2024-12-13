3dd4b2d471a0a47d198fe5019d98a6343cd965ea
William Sylvestre <farakwarts@gmail.com>
2024-12-12 22:48:14 -0500

    Add Flashlight Script into Items Folder

--- Assets/Scripts/Items/Flashlight.cs
+++ Assets/Scripts/Items/Flashlight.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class Flashligh : MonoBehaviour
+public class Flashlight : MonoBehaviour
 {
     [SerializeField] GameObject lightObj;
     public AudioClip[] interactSounds;
