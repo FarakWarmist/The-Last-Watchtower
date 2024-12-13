a62628991b8cb147193bf56203092b36b1fd4a59
William Sylvestre <farakwarts@gmail.com>
2024-12-12 23:01:55 -0500

    Add Electricity Folder into Scripts Folder

---
+++ Assets/Scripts/Electricity/LightSwitch.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour, IInteractable
+{
+    public bool switchOn;
+    public Light roomLight;
+    Animator animator;
+    AudioSource audioSource;
+    [SerializeField] Generator generator;
+    Sleep sleep;
+    [SerializeField] CharacterText characterText;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        switchOn = true;
+
+        sleep = FindAnyObjectByType<Sleep>();
+    }
+
+    public void Interact()
+    {
+        if (switchOn)
+        {
+            Overheated();
+        }
+
+        if (generator.energyLevel == 3)
+        {
+            switchOn = !switchOn;
+        }
+        else
+        {
+            switchOn = false;
+            if (!sleep.isDay)
+            {
+                characterText.enabled = true;
+                characterText.newText =
+@"Il n'y a plus d'énergie.
+J'ai besoin de repartir la génératrice.";
+            }
+        }
+
+        audioSource.Play();
+    }
+
+    private void Overheated()
+    {
+        bool overheated = Random.Range(0f, 1f) <= 0.2f;
+        if (overheated)
+        {
+            generator.energyLevel = 0;
+            switchOn = false;
+        }
+    }
+
+    private void Update()
+    {
+        roomLight.enabled = switchOn;
+        animator.SetBool("Switch", switchOn);
+    }
+}
