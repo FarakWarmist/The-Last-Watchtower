586ab3e10b7387f96576c1f9e5329ec93f952219
William Sylvestre <farakwarts@gmail.com>
2024-12-20 13:59:53 -0500

    Update MainMenuManager
    Add mostly the Retry menu

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -8,33 +8,43 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     public Button playButton;
-    public Button optionsButton;
+    public Button settingsButton;
     public Button quitButton;
     public Button backButton;
+    public Button retryButton;
+    public Button notRetryButton;
+
     public CinemachineCamera mainMenuCam;
     public CinemachineCamera playerCam;
+
     public Animator animator;
+
     public Canvas mainMenu;
     public Canvas settings;
+    public Canvas deathMenu;
+
     public GameObject introText;
     public CheckCursor cursorState;
-    public GameObject icons;
+    public Canvas icons;
     Player player;
+    Reset reset;
 
     private void OnEnable()
     {
         player = FindAnyObjectByType<Player>();
+        reset = FindAnyObjectByType<Reset>();
         player.enabled = false;
-        icons.SetActive(false);
+        icons.enabled = false;
 
         StartCoroutine(StartMainMenu());
 
         playButton.onClick.AddListener(OnPlayButtonClicked);
-        optionsButton.onClick.AddListener(OnOptionsButtonClicked);
+        settingsButton.onClick.AddListener(OnOptionsButtonClicked);
         quitButton.onClick.AddListener(OnQuitButtonClicked);
         backButton.onClick.AddListener(OnBackButtonClicked);
+        retryButton.onClick.AddListener(OnRetryButtonClicked);
+        notRetryButton.onClick.AddListener(OnQuitButtonClicked);
     }
-
 
     private void OnPlayButtonClicked()
     {
@@ -55,6 +65,12 @@ public class MainMenuManager : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(null);
     }
 
+    private void OnRetryButtonClicked()
+    {
+        StartCoroutine(Retry());
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
     private void OnBackButtonClicked()
     {
         Debug.Log("Back");
@@ -71,7 +87,7 @@ public class MainMenuManager : MonoBehaviour
         mainMenu.enabled = true;
         animator.SetBool("Fade", false);
         yield return new WaitForSeconds(0.01f);
-        icons.SetActive(true);
+        icons.enabled = true;
         cursorState.needCursor++;
     }
 
@@ -85,6 +101,33 @@ public class MainMenuManager : MonoBehaviour
         mainMenu.enabled = false;
         yield return new WaitForSeconds(1f);
         animator.SetBool("Fade", false);
-        introText.SetActive(true);
+        //introText.SetActive(true);
+        player.enabled = true;
+    }
+
+    IEnumerator Retry()
+    {
+        icons.enabled = false;
+        cursorState.needCursor = 0;
+        animator.speed = 0.25f;
+        animator.SetBool("Fade", true);
+
+        yield return new WaitForSeconds(1.5f);
+
+        deathMenu.enabled = false;
+        mainMenuCam.enabled = false;
+        playerCam.enabled = true;
+        icons.enabled = true;
+        reset.ResetTheGame();
+
+        yield return new WaitForSeconds(0.5f);
+
+        MouseLook mouseLook = FindAnyObjectByType<MouseLook>();
+        mouseLook.enabled = true;
+        player.enabled = true;
+        animator.SetBool("Fade", false);
+
+        yield return new WaitForSeconds(1f);
+        animator.speed = 1;
     }
 }
