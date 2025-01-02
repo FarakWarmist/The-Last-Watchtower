6b71dfdfbaac1d7b701e729c9363daa7ec4684ac
William Sylvestre <farakwarts@gmail.com>
2025-01-02 15:46:27 -0500

    Fix Bugs w/ MessageRadioManager

--- Assets/Scripts/Radio/MessageRadioManager.cs
+++ Assets/Scripts/Radio/MessageRadioManager.cs
@@ -23,10 +23,10 @@ public class MessageRadioManager : MonoBehaviour
     public int lastPath;
 
     public float time;
-    public float shortTime = 12f;
-    public float mediumTime = 30f;
-    public float longTime = 60f;
-    public float quickTime = 18;
+    public float shortTime = 15f;
+    public float mediumTime = 35f;
+    public float longTime = 75f;
+    public float quickTime = 90f;
     public float bullets = 3;
 
     [SerializeField] GameObject monsters;
@@ -278,44 +278,44 @@ public class MessageRadioManager : MonoBehaviour
                     message = @"J'ai perdu mon groupe et j'aurais besoin d'un coup de main.";
                     break;
                 case 2: // A
-                    message = @"Par pitié ! Est-ce que quelqu'un m'entend?";
+                    message = @"Par pitié! Est-ce que quelqu'un m'entend?";
                     answer1 = @"Oui! Je suis là?";
-                    answer2 = @"Qu'est-ce qui ce passe?";
+                    answer2 = @"Qu'est-ce qui se passe?";
                     break;
                 // Answer 1A & 2A
                 case 3f:
-                    message = @"Ho mon Dieu! Ça l'a marcher !";
+                    message = @"Ho mon Dieu! Ça a marché!";
                     break;
                 case 4f: // B
-                    message = @"Hum, pardon. Je suis Alex, explorateur de 2ème grade, et je me retrouve dans une situation compliqué.";
-                    answer1 = @"Je suis Éron... le nouveau Watcher";
-                    answer2 = @"Que c'est-il passé?";
+                    message = @"Hum, pardon. Je suis Alex, explorateur de 2ème grade, et je me trouve dans une situation compliquée.";
+                    answer1 = @"Je suis Éron... le nouveau Watcher.";
+                    answer2 = @"Que s'est-il passé?";
                     break;
                 // Answer 1B
                 case 5f:
-                    message = @"Enchanté Ér.. Attend! Un Watcher?!";
+                    message = @"Enchanté Ér.. Attends! Un Watcher?!";
                     break;
                 case 6f:
-                    message = @"Je pensait que la dernière Tour avait avait été détruite.";
+                    message = @"Je pensais que la dernière Tour avait avait été détruite.";
                     break;
                 case 7f:
                     message = @"Je ne vais pas me plaindre.
-Un Watcher est exactement ce qu'il me faut !";
+Un Watcher est exactement ce qu'il me faut!";
                     break;
                 // Answer 2B
                 case 8f:
                     message = @"Moi et mon groupe avons été pris en embuscade par un Rooted Priest et ses Rooted Ghouls.";
                     break;
                 case 9f:
-                    message = @"Dans la panique, on a été séparé.
-J'ai réussit à trouver refuge dans une Ruine où j'ai trouver cette radio avec les reste d'un Explorateur malchanceux.";
+                    message = @"Dans la panique, nous nous sommes séparés.
+J'ai réussi à trouver refuge dans une Ruine où j'ai trouvé cette radio avec les reste d'un Explorateur malchanceux.";
                     break;
                 case 10f:
-                    message = @"L'un de mes collèges avait la carte.
-Et sans repère, autant dire que je suis mort.";
+                    message = @"L'un de mes collègues avait la carte.
+Et sans repères, autant dire que je suis mort.";
                     break;
                 case 11f:
-                    message = @"Mais si tu es dans une tour, ça voudrait dire que tu devrais en avoir une!";
+                    message = @"Mais si tu es dans une Tour, ça voudrait dire que tu devrais en avoir une!";
                     break;
                 case 12f: // C
                     message = @"Avant qu'on se lance là-dedans, connais-tu la fonction d'un Watcher?";
@@ -327,7 +327,7 @@ Et sans repère, autant dire que je suis mort.";
                     message = @"Pas de problème, je te résume tout ça.";
                     break;
                 case 14f:
-                    message = @"Dans un moment, je vais te donner des points de repère afin que tu me localise sur la carte.";
+                    message = @"Dans un moment, je vais te donner des points de repère afin que tu me localises sur la carte.";
                     break;
                 case 15f:
                     message = @"Une fois fait, tu dois m'indiquer le chemin vers le Camp le plus près.
@@ -338,25 +338,25 @@ Et il n'y a pas de retour possible, alors soit sûr de toi.";
                     break;
                 case 17f:
                     message = @"Tu devrais avoir un terminal à ta porter.
-Utilise le pour m'indiquer comment m'en sauver.";
+Utilise le pour m'indiquer comment m'en sortir si j'en rencontre.";
                     break;
                 case 18f:
-                    message = @"Maintenant que tout est expliquer, temps de se mettre au travail.";
+                    message = @"Maintenant que tout est expliqué, il est temps de se mettre au travail.";
                     break;
                 // Answer 2C
                 case 18.1f:
                     message = @"Bien, ça me rassure.";
                     break;
                 case 19f:
-                    message = @"Durant l'attque, j'ai couru Sud-Est.
-J'ai croisé un arbre à 3 troncs, un rocher avec une spiralle et je me suis abrité dans une Ruine.";
+                    message = @"Durant l'attque, j'ai couru verd le Sud-Est.
+J'ai croisé un arbre à 3 troncs, un rocher avec une spirale et je me suis abrité dans une Ruine.";
                     break;
                 case 20f:
-                    message = @"Fis toi à la carte et indique moi le chemin vers le camps le plus proche.";
+                    message = @"Fie-toi à la carte et indique-moi le chemin vers le Camp le plus proche.";
                     break;
                 case 21f: // D
-                    message = @"N'oublie pas : Sud-Est, Arbre à 3 Troncs, Rocher avec Spiralle et Ruine.
-Quel direction je devrais prendre?";
+                    message = @"N'oublies pas : Sud-Est, Arbre à 3 Troncs, Rocher avec Spirale et Ruine.
+Quelle direction devrais-je prendre?";
                     answer1 = @"Nord-Est";
                     answer2 = @"Ouest";
                     break;
@@ -366,7 +366,7 @@ Quel direction je devrais prendre?";
                     break;
                 case 23f:
                     message = @"Je te rappelle dans un moment.
-En attendant, fait attention à toi.";
+En attendant, fais attention à toi.";
                     break;
                 case 24f:
                     message = @"À ce qu'on raconte, les Tours ne sont pas un endroit très sûr.";
@@ -390,44 +390,44 @@ En attendant, fait attention à toi.";
                     break;
                 // Answer 1A & 2A
                 case 2:
-                    message = @"Tu a du rencontrer des Rooted Ghouls.";
+                    message = @"Tu a dû rencontrer des Rooted Ghouls.";
                     break;
                 case 3:
                     message = @"Ces choses sont les esclaves des Rooted Priests.
-Des gens comme toi et moi qui ont eu le malheur de se retrouver transformer en... ça.";
+Des gens comme toi et moi qui ont eu le malheur de se retrouver transformés en... ça.";
                     break;
                 case 4:
-                    message = @"Tu pourras trouver plus d'info sur ton Terminal.";
+                    message = @"Tu pourras trouver plus d'infos sur ton Terminal.";
                     break;
                 case 5:
-                    message = @"Ne les laisse pas te tourmanté. 
+                    message = @"Ne les laisse pas te tourmenter. 
 Ils essaient de te déstabiliser pour frapper au bon moment.";
                     break;
                 case 6:
-                    message = @"Assure-toi juste de fréquemment regarder s'il y en a dans les alentours et les chasser.";
+                    message = @"Assure-toi juste de regarder fréquemment s'il y en a dans les alentours et de les chasser.";
                     break;
                 case 7:
-                    message = @"Et ne les laisse surtout pas renter dans la tour, ou tu finiras probablement comme eux.";
+                    message = @"Et ne les laisse surtout pas entrer dans la Tour, ou tu finiras probablement comme eux.";
                     break;
                 case 8:
-                    message = @"Maintenant, c'est moi qui aurait besoin de tes conseils.";
+                    message = @"Maintenant, c'est moi qui aurais besoin de tes conseils.";
                     break;
                 case 9:
-                    message = @"J'ai suivis le chemin que tu m'as dit sans difficulté. 
+                    message = @"J'ai suivi le chemin que tu m'as indiqué sans difficulté. 
 Mais je commence à entendre des sons qui n'annoncent rien de bon.";
                     break;
                 case 10: // B
                     message = @"Je viens de passer à côté d'une ruine.
 Une ancienne maison, je pense. 
-Quel chemin je devrais prendre?";
+Quel chemin devrais-je prendre?";
                     answer1 = @"Hum... Mauvaise nouvelle.";
                     answer2 = @"...";
                     break;
                 // Answer 1B
                 case 11: // C
-                    message = @"Quoi. Quoi! Qu'est-ce qu'il y a?";
+                    message = @"Quoi! Qu'est-ce qu'il y a?";
                     answer1 = @"Heu... Non, rien. Fausse alerte.";
-                    answer2 = @"Les deux chemin sont... non recommandé.";
+                    answer2 = @"Les deux chemins sont... non recommandés.";
                     break;
                 // Answer 2C
                 case 12:
@@ -454,13 +454,13 @@ Quel chemin je devrais prendre?";
                     break;
                 // Answer 1D & 2D
                 case 19:
-                    message = @"Très bien. C'est partie!";
+                    message = @"Très bien. C'est parti!";
                     break;
                 case 20:
-                    message = @"Je te contacte dans un momment.";
+                    message = @"Je te contacte dans un moment.";
                     break;
                 case 21:
-                    message = @"Courage Watcher !";
+                    message = @"Courage, Watcher !";
                     break;
                 default:
                     message = "";
@@ -472,27 +472,30 @@ Quel chemin je devrais prendre?";
             switch (messagePart)
             {
                 case 0: // A
-                    message = @"Hé, buddy! Toujours en un seul morceaux?";
+                    message = @"Hé, buddy! 
+Toujours en un seul morceaux?";
                     answer1 = @"Yep! Besoin de mon aide?";
                     answer2 = @"Il me resque que ma tête, mais tout roule.";
                     break;
                 // Answer A1
                 case 0.1f:
-                    message = @"Ho! Non, non. C'est juste que...";
+                    message = @"Ho! Non, non.
+C'est juste que...";
                     break;
                 // Answer A2
                 case 0.2f:
-                    message = @"Ha ha ! C'est qu'il a de l'humour le nouveau.";
+                    message = @"Ha ha! 
+C'est qu'il a de l'humour le nouveau!";
                     break;
                 case 1f:
-                    message = @"Se retrouver seul dans cette forêt maudite, avec tout ces bruits, ça commence à gruger ma santé mentale.";
+                    message = @"Se retrouver seul dans cette forêt maudite, avec tous ces bruits, ça commence à gruger ma santé mentale.";
                     break;
                 case 2:
                     message = @"Je commence même à avoir la sérieuse sensation que quelque chose m'observe depuis un moment...";
                     break;
                 case 3: // B
-                    message = @"Alors si je n'ai personne à qui parler, je sens que je vais peter un cable.";
-                    answer1 = @"Je comprend. Je suis là au besoin.";
+                    message = @"Alors si je n'ai personne à qui parler, je sens que je vais péter un câble.";
+                    answer1 = @"Je comprends. Je suis là au besoin.";
                     answer2 = @"...";
                     break;
                 // Answer B1
@@ -508,21 +511,21 @@ Quel chemin je devrais prendre?";
                     message = @"Mais se retrouver tout seul et en pleine nuit, c'est une première.";
                     break;
                 case 7:
-                    message = @"On ne contait pas rester plus tard que le coucher du soleil, mais...";
+                    message = @"On ne comptait pas rester plus tard que le coucher du soleil, mais...";
                     break;
                 case 8:
                     message = @"On avait trouvé ce centre de recherche qui ne figurait pas sur la carte.
-C'était quoi déjà le nom?... C'était un truc comme ""Wonder-""...";
+C'était quoi déjà le nom?... Un truc comme ""Wonder-""...";
                     break;
                 case 9:
                     message = @"""WonderLife""! C'est ça!
-On y a trouvez des documents à propos d'une entité surnommée la ""Dryádos"" qui semblait être un des sujet de test du centre.";
+On y a trouvé des documents à propos d'une entité surnommée la ""Dryádos"" qui semblait être un des sujets de test du centre.";
                     break;
                 case 10:
-                    message = @"L'entité semblait rétissant à l'idée d'intéragir avec les membre du centre.";
+                    message = @"L'entité était réticente à l'idée d'interagir avec les membres du centre.";
                     break;
                 case 11:
-                    message = @"Les documents m'entionnait qu'ils ont utilisés six enfants pour servir de communication entre elle et les chercheurs.";
+                    message = @"Les documents mentionnaient qu'ils ont utilisé six enfants qui servaient de communication entre elle et les chercheurs.";
                     break;
                 case 12:
                     message = @"Mais il y a eu un accident au centre qui a causé la perte de ...";
@@ -542,7 +545,7 @@ On y a trouvez des documents à propos d'une entité surnommée la ""Dryádos"" qui 
                 case 15: // D
                     message = @"Un False Deer.";
                     answer1 = @"Un quoi?";
-                    answer2 = @"Qu'est-ce que tu raconte?";
+                    answer2 = @"Qu'est-ce que tu racontes?";
                     time = shortTime;
                     break;
                 // Answer D1 & D2
@@ -767,17 +770,20 @@ PAR PITIÉ AIDE-M...";
             switch (messagePart)
             {
                 case 0: // A
-                    message = @"Hé, buddy! Toujours en un seul morceaux?";
+                    message = @"Hé, buddy!
+Toujours en un seul morceaux?";
                     answer1 = @"Yep! Besoin de mon aide?";
                     answer2 = @"Il me resque que ma tête, mais tout roule.";
                     break;
                 // Answer A1
                 case 0.1f:
-                    message = @"Ho! Non, non. C'est juste que...";
+                    message = @"Ho! Non, non.
+C'est juste que...";
                     break;
                 // Answer A2
                 case 0.2f:
-                    message = @"Ha ha ! C'est qu'il a de l'humour le nouveau.";
+                    message = @"Ha ha!
+C'est qu'il a de l'humour le nouveau!";
                     break;
                 case 1f:// B
                     message = @"Connaissais-tu bien les autres Watchers?";
@@ -820,7 +826,7 @@ Et pourtant, te voilà!";
                 case 15: // D
                     message = @"Un False Deer.";
                     answer1 = @"Un quoi?";
-                    answer2 = @"Qu'est-ce que tu raconte?";
+                    answer2 = @"Qu'est-ce que tu racontes?";
                     time = shortTime;
                     break;
                 // Answer D1 & D2
